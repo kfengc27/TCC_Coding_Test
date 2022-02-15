4be0f5a406099405d9340557b2e8dbcf5b2b0d88
Kevin Feng <kfengc27@gmail.com>
2022-02-15 22:45:07 +1100

    add user type

--- TCC_Interview/user_management/models.py
+++ TCC_Interview/user_management/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 
 # Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    clinicians = models.BooleanField(default=False) # a clinicians 
+    patients = models.BooleanField(default=False) # a patients 
+    admin = models.BooleanField(default=False) # a superuser
+
+    @property
+    def is_patients(self):
+        "Is the user a patients"
+        return self.patients
+
+    @property
+    def is_clinicians(self):
+        "Is the user a member of staff"
+        return self.clinicians 
+
+    @property
+    def is_admin(self):
+        "Is the user a admin member?"
+        return self.admin
