faefb17a268d7f0f117f9570e1ea9a7392f69984
Kevin Feng <kfengc27@gmail.com>
2022-02-19 18:23:19 +1100

    solve conflicts

--- TCC_Interview/TCC_Interview/settings.py
+++ TCC_Interview/TCC_Interview/settings.py
@@ -11,12 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-<<<<<<< HEAD
-import django_heroku
-
-=======
 import os 
->>>>>>> dev
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
